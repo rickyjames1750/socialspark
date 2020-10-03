4c96bbfb422a4f5047115a2e8fdf9fbda9e83bf8
Ricky J. Sparks <rickyjames1750@gmail.com>
2020-10-03 00:24:52 -0700

    This commit will push the final code build

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Ricky Sparks on 10/2/20.
+//  Copyright © 2020 Ricky J. Sparks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

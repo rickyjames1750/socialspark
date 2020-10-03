3c1eb521b6b9a9540530e1bff4c6b7a707b17e69
Ricky J. Sparks <rickyjames1750@gmail.com>
2020-10-03 11:21:46 -0700

    This commit will add stack views to social spark

---
+++ StackViewExcercise/StackViewExcercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackViewExcercise
+//
+//  Created by Ricky Sparks on 10/3/20.
+//  Copyright © 2020 Ricky J. Sparks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
